d9b5bf7ba98fc0546cda04dcf4d14148ab9dc22e
FernandoDeleg <56567621+FernandoDeleg@users.noreply.github.com>
2021-04-12 23:06:50 -0500

    Add files via upload

--- API/js/scripts.js
+++ API/js/scripts.js
@@ -1,44 +1,22 @@
-var app = new Vue({
-    el: "#app",
-    data() {
-      return {
-          query: null,
-          results: null,
-          apikey: 'f6b43635'
-      }
-    },
-    created() {
-      this.getMovies();
-    },
-    methods: {
-      getMovies() {
-        axios.get(`http://www.omdbapi.com/?s=${ this.query }&apikey=${ this.apikey }`)
-          .then(response => {
-            console.log(response.data.Search)
-  
-            results = response.data.Search
-  
-            let html = '';
-  
-            results.forEach(result => {
-              html += `
-                <div class="row">
-                  <div class="col-lg-12 col-sm-12 col-md-6" movies>
-                  <div class="card style="width: 18rem;">
-                    <img src="${ result.Poster }" class="card-image-top img-fluid">
-                    <div class="card-body">
-                      <h4 class="card-title">${result.Title }</h4>
-                      <p class="card-text">Year: ${ result.Year }</p>
-                    </div>
-                  </div>
-                  </div>
+
+var contenido = document.querySelector('#contenido')
+        function traer() {
+            fetch('https://randomuser.me/api/')
+            .then(res => res.json())
+            .then(data => {
+
+              
+                console.log(data.results['0'])
+                contenido.innerHTML = `
+                <div class="foto">
+                <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle" name> 
                 </div>
-              `
+                <div class"row">
+                <p>Nombre: ${data.results['0'].name.first}</p>
+                <p>Apellido: ${data.results['0'].name.last}</p>
+                <p>email: ${data.results['0'].email}</p>
+                </div>
+                `
             });
-  
-            document.getElementById('movies').innerHTML = html;
-  
-          })
-      }
-    }
-  })
+            
+        }
